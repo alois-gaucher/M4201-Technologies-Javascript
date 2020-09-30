db81081b8e0541fcb926db7ce34695a232c302ad
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-09-30 10:54:41 +0200

    * TD4 Terminé

--- exercice1-td4/server03.js
+++ exercice1-td4/server03.js
@@ -2,13 +2,22 @@ var http = require('http');
 var url = require('url');
 var port = 8080
 http.createServer(function (request, response) {
-    var message = url.parse(request.url).pathname.toLowerCase();
-    console.log(request.url + ' requested')
-    if (message == "/welcome") {
+    var requested = url.parse(request.url).pathname.toLowerCase();
+    console.log(requested.url + ' requested')
+    if (requested == "/welcome") {
         message = "Bienvenue sur le site";
     }
-    if (message == "/catalogue") {
+    else if (requested == "/catalogue") {
         message = "Voici le catalogue";
+    }
+    else if (requested == "/hello") {
+        message = "Hello !";
+    }
+    else if (requested == "/byebye") {
+        message = "Goodbye !";
+    }
+    else {
+        message = "Route not found, sorry...";
     }
     response.writeHead(200);
     console.log('Sent : ' + message);
