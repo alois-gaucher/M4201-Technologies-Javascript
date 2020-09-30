280cb1ca8f5d72f432e567b8db9beb843b7b406a
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-09-30 10:48:52 +0200

    * Exercice 1 TD4

--- exercice1-td4/server03.js
+++ exercice1-td4/server03.js
@@ -2,15 +2,16 @@ var http = require('http');
 var url = require('url');
 var port = 8080
 http.createServer(function (request, response) {
-    var message = url.parse(request.url).pathname;
-    console.log(request.url)
-    if (message ==  "/Welcome") {
+    var message = url.parse(request.url).pathname.toLowerCase();
+    console.log(request.url + ' requested')
+    if (message == "/welcome") {
         message = "Bienvenue sur le site";
     }
-    if (message ==  "/Catalogue") {
+    if (message == "/catalogue") {
         message = "Voici le catalogue";
     }
     response.writeHead(200);
+    console.log('Sent : ' + message);
     response.write("<html><body><h1>" + message + "</h1></body></html>");
     response.end();
 }).listen(port);
