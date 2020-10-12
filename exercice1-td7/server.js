f623ccd1e90e746c202f043efd6db4bf53c94788
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-10-12 08:35:48 +0200

    * Modification du nom de server01.js

--- exercice1-td7/server.js
+++ exercice1-td7/server.js
@@ -5,11 +5,19 @@ var port=45678;
 
 var app=express();
 
+/* Import de ejs */
 app.set('view engine', 'ejs');
 
+/* Prise en compte du dossier assets comme dossier statique */
 app.use('/assets', express.static('assets'));
 
+/* Route / */
 app.get('/', function (req, res) {
+  res.render('index');
+});
+
+/* Route /articlesList */
+app.get('/articlesList', function (req, res) {
   res.render('index');
 });
 
