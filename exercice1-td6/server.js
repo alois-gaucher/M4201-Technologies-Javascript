a373cb5251d4ea9e3fa5ddf05d770c3eb24f0613
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-10-14 08:26:31 +0200

    * Création du socket

--- exercice1-td6/server.js
+++ exercice1-td6/server.js
@@ -1,1 +1,7 @@
-var socketio = require("socket.io").listen(8080);
+var port = 8080;
+console.log("Server listening on " + port);
+var socketio = require("socket.io").listen(port);
+socketio.sockets.on("connection", function (client) {
+    var address = socketio.handshake.address;
+    console.log('New connection from ' + address.address + ':' + address.port);
+});
