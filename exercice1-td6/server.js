9ae82f46f5d6a801597451cdc7b36a6784de051b
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-10-14 08:28:03 +0200

    * Commentaires

--- exercice1-td6/server.js
+++ exercice1-td6/server.js
@@ -1,7 +1,13 @@
+/* Définition du port */
 var port = 8080;
 console.log("Server listening on " + port);
+
+/* Import de socket.io et lancement du serveur */
 var socketio = require("socket.io").listen(port);
+
 socketio.sockets.on("connection", function (client) {
+    /* Récupération des infos du client */
     var address = socketio.handshake.address;
+    /* Log des infos du client */
     console.log('New connection from ' + address.address + ':' + address.port);
 });
